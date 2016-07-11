247fb94c4315f496bc5767d6f717832381560755
丁一鸣 <315129552@qq.com>
2016-07-11 20:33:31 +0800

    main.js添加 vue-resource配置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 var Vue = require('vue')
+var VueResource = require('vue-resource')
 var VueRouter = require('vue-router')
 var routerConfig = require('./router')
 var app = require('./app')
 Vue.use(VueRouter)
-
+Vue.use(VueResource)
 var router = new VueRouter()
 
 routerConfig(router)
--- src/router.js
+++ src/router.js
@@ -1,21 +1,15 @@
 module.exports = function(router) {
 	router.map({
-		// '/': {
-		// 	component: function (resolve) {
-  //   			require(['./view1'], resolve) // 异步加载view1
-  //   		}
-		// },
-		// '/v2': {
-		// 	component: function (resolve) {
-  //   			require(['./view2'], resolve)
-  //   		}
-		// }
 		'/': {
-			component: require('./view1')
+			component: function (resolve) {
+    			require(['./view1'], resolve) // 异步加载view1
+    		}
 		},
 		'/v2': {
-			component: require('./view2')
-		}		
+			component: function (resolve) {
+    			require(['./view2'], resolve)
+    		}
+		}
 	})
 	router.redirect({
 		'*': '/'

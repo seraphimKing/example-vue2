5988658ab684a70926a466c6069b68fe252ee63f
丁一鸣 <315129552@qq.com>
2016-07-24 04:24:13 +0800

    update

--- vuec/new.sh
+++ vuec/new.sh
@@ -6,6 +6,6 @@ mkdir $1 && cd $1 && touch index.html && touch index.css && touch index.js && to
 echo "require(\"./index.css\"); \nmodule.exports = { \n    template: require(\"./index.html\") \n}" > index.js
 
 # 将组件名称和新建时间写入组件的说明文档中
-echo "# $1 \n> 说明: $2 \n* 更新时间:  $(date +%Y-%m-%d-%H:%M:%S) \n  * 当前可复用等级:$3" > readme.md
+echo "# $1 \n> 说明: $2 \n \n* 更新时间:  $(date +%Y-%m-%d-%H:%M:%S) \n * 当前可复用等级:$3" > readme.md
 # 提示成功
 echo "完成:组件$1初始化成功!  $(date +%Y-%m-%d-%H:%M:%S)"

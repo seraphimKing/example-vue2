b62ce5b6957e3be5b5c1a991bdbdd5146dd110e2
dingyiming <315129552@qq.com>
2016-07-29 11:25:37 +0800

    update

--- webpack/dev.js
+++ webpack/dev.js
@@ -3,44 +3,44 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 module.exports = {
-  entry: {
-    app: './src/main.js',
-    vendor: ['vue', 'vue-router', 'vue-resource']
-  },
-  output: {
-    path: './dist',
-    filename: 'app.js',
-    publicPath: ''
-  },
-  module: {
-    loaders: [{
-      test: /\.(png|jpg|jpeg|gif)$/,
-      loader: 'url?limit=10000&name=images/[name].[ext]'
-    }, {
-      test: /\.(woff|svg|ttf|eot)$/,
-      loader: 'url?limit=10000&name=iconfont/[name].[ext]'
-    }, {
-      test: /\.html$/,
-      loader: 'html-loader'
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64]')
-    }]
-  },
-  plugins: [
-    new ExtractTextPlugin("app.css"),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin('vendor',
-      'vendor_vue_vuerouter.js'),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html',
-      inject: true
-    })
-  ]
+    entry: {
+        app: './src/main.js',
+        vendor: ['vue', 'vue-router', 'vue-resource']
+    },
+    output: {
+        path: './dist',
+        filename: 'app.js',
+        publicPath: ''
+    },
+    module: {
+        loaders: [{
+            test: /\.(png|jpg|jpeg|gif)$/,
+            loader: 'url?limit=10000&name=images/[name].[ext]'
+        }, {
+            test: /\.(woff|svg|ttf|eot)$/,
+            loader: 'url?limit=10000&name=iconfont/[name].[ext]'
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64]')
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("app.css"),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin('vendor',
+            'vendor_vue_vuerouter.js'),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: true
+        })
+    ]
 }
--- webpack/prod.js
+++ webpack/prod.js
@@ -2,37 +2,43 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin")
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var webpack = require('webpack')
 module.exports = {
-	entry:
-	 {
-       app: './src/main.js',
-       vendor: ['vue','vue-router','vue-resource']
+    entry: {
+        app: './src/main.js',
+        vendor: ['vue', 'vue-router', 'vue-resource']
     },
-	output: {
-		path: './dist',
-		filename: 'js/app.[hash].js',
-		publicPath: '' // 可以在这里配置对应的输出CDN路径如 http://localhost:8080/
-	},
-	module: {
-		loaders: [
-		  	{ test: /\.(png|jpg|jpeg|gif)$/, loader: 'url?limit=10000&name=images/[name].[ext]' },
-		  	{ test: /\.(woff|svg|ttf|eot)$/, loader: 'url?limit=10000&name=iconfont/[name].[ext]' },
-			{ test: /\.html$/,loader: 'html-loader' },
-			{ test: /\.css$/,loader: ExtractTextPlugin.extract("style-loader!css-loader") }
-		]
-	},
-	plugins: [
+    output: {
+        path: './dist',
+        filename: 'js/app.[hash].js',
+        publicPath: '' // 可以在这里配置对应的输出CDN路径如 http://localhost:8080/
+    },
+    module: {
+        loaders: [{
+            test: /\.(png|jpg|jpeg|gif)$/,
+            loader: 'url?limit=10000&name=images/[name].[ext]'
+        }, {
+            test: /\.(woff|svg|ttf|eot)$/,
+            loader: 'url?limit=10000&name=iconfont/[name].[ext]'
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64]')
+        }]
+    },
+    plugins: [
         new ExtractTextPlugin("css/app.[hash].css"),
         new webpack.optimize.UglifyJsPlugin({
-	      compress: {
-	        warnings: false
-	      }
-	    }),
-	    new webpack.optimize.CommonsChunkPlugin('vendor',  'js/vendor_vue_vuerouter_vueresource.js'),
-	    // https://github.com/ampedandwired/html-webpack-plugin
-	    new HtmlWebpackPlugin({
-	      filename: 'html/index.html',
-	      template: './src/index.html',
-	      inject: true
-	    })
-  ]
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'js/vendor_vue_vuerouter_vueresource.js'),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'html/index.html',
+            template: './src/index.html',
+            inject: true
+        })
+    ]
 }

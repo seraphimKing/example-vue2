a1c9b4b734d179f97326561e79ddfe9404b325ed
dingyiming <315129552@qq.com>
2016-09-26 10:59:19 +0800

    fix lazyload

--- template/client/router.js
+++ template/client/router.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 // 引入页面
-const Home = r => require.ensure([], () => r(require('../views/Home')), 'home');
+const Home = r => require.ensure([], () => r(require('views/Home')), 'home');
 
 export default new VueRouter({
   mode: 'hash',
